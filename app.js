9349627c50b1bda33131bb0454006d0369ede9f9
watidy-octivault <mahmoud.watidy+octivault@gmail.com>
2022-07-06 18:35:46 +0200

    updated

--- app.js
+++ app.js
@@ -39,16 +39,16 @@ const remote = `https://${ USER }:${ PASS }@${ REPO }`;
 
 (async () => {
   simpleGit().addConfig('user.email','mahmoud.watidy+octivault@gmail.com');
-simpleGit().addConfig('user.name','watidy-octivault');
+  simpleGit().addConfig('user.name','watidy-octivault');
 
-if (fs.existsSync(dir)) {
-  console.log('Directory exists!');
-} else {
-  await git().silent(true)
-  .clone(remote ,'octiClone')
-  .then(() => console.log('finished'))
-  .catch((err) => console.error('failed: ', err));
-}
+  if (fs.existsSync(dir)) {
+    console.log('Directory exists!');
+  } else {
+    await git().silent(true)
+    .clone(remote ,'octiClone')
+    .then(() => console.log('finished'))
+    .catch((err) => console.error('failed: ', err));
+  }
 
 
 // Add all files for commit
