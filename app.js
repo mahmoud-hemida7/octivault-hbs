ca52cefaad99bef245416d0f10f2d47e4cfcddc8
watidy-octivault <mahmoud.watidy+octivault@gmail.com>
2022-07-17 18:29:19 +0200

    updated

--- app.js
+++ app.js
@@ -101,11 +101,13 @@ async function getStoryblok(req, res, next){
     }, (failed) => {
         console.log('failed commmit',failed);
     });
-    //   to online repository
-    await git().addRemote('origin', 'https://github.com/mahmoud-hemida7/octivault-hbs.git')
 
     // Finally push to online repository
-    await git().push(['-u', 'origin', 'main'], () => console.log('done'));
+    await git().push('origin','main').then((success) => {
+        console.log(' successfully pushed');
+    },(failed)=> {
+        console.log(' push failed');
+    }); 
     
   } catch (error) {
     res.send(error)
