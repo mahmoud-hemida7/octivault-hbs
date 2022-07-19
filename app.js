f055bfa07c6b59dc139c4bc7388814245cc0c2e3
watidy-octivault <mahmoud.watidy+octivault@gmail.com>
2022-07-19 02:54:28 +0200

    updated

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 var Handlebars = require('handlebars');
 const express = require('express');
 const simpleGit = require('simple-git');
-const git = require('simple-git');
 
 const StoryblokClient = require('storyblok-js-client');
 
@@ -37,39 +36,39 @@ const remote = `https://${ USER }:${ PASS }@${ REPO }`;
 
  //let git = simpleGit();
 
-// (async () => {
-//   simpleGit().addConfig('user.email','mahmoud.watidy+octivault@gmail.com');
-//   simpleGit().addConfig('user.name','watidy-octivault');
+ (async () => {
+  simpleGit().addConfig('user.email','mahmoud.watidy+octivault@gmail.com');
+  simpleGit().addConfig('user.name','watidy-octivault');
 
-//   if (fs.existsSync(dir)) {
-//     console.log('Directory exists!');
-//   } else {
-//     await git().silent(true)
-//     .clone(remote ,'octiClone')
-//     .then(() => console.log('finished'))
-//     .catch((err) => console.error('failed: ', err));
-//   }
+  if (fs.existsSync(dir)) {
+    console.log('Directory exists!');
+  } else {
+    await simpleGit().silent(true)
+    .clone(remote ,'octiClone')
+    .then(() => console.log('finished'))
+    .catch((err) => console.error('failed: ', err));
+  }
 
-//  // clone repo from remote
-//   // --------------------------------------------------
+ // clone repo from remote
+  // --------------------------------------------------
 
-//   // const SimpleGit = simpleGit({ 
-//   //   baseDir: process.cwd() + '/build',
-//   //   binary: 'git',
-//   //   maxConcurrentProcesses: 6,
-//   //   config: [
-//   //     'http.proxy=someproxy'
+  // const SimpleGit = simpleGit({ 
+  //   baseDir: process.cwd() + '/build',
+  //   binary: 'git',
+  //   maxConcurrentProcesses: 6,
+  //   config: [
+  //     'http.proxy=someproxy'
       
-//   //   ]});
+  //   ]});
   
-//   //  ===>  TODO FOR NEXT TIME  <====
+  //  ===>  TODO FOR NEXT TIME  <====
   
-//   // - FINISH GIT OPS
-//   // - HEROKU
-//   // - ERROR HANDLING
+  // - FINISH GIT OPS
+  // - HEROKU
+  // - ERROR HANDLING
     
     
-// })();
+})();
 
 /////////////Configure and initialize storyblok client///////////
 
@@ -87,7 +86,7 @@ async function getStoryblok(req, res, next){
 
   try {
     // Add all files for commit
-    await git().add('./*').then(
+    await simpleGit().add('./*').then(
       (addSuccess) => {
           console.log('addSuccess',addSuccess);
       }, (failedAdd) => {
@@ -95,7 +94,7 @@ async function getStoryblok(req, res, next){
     });
 
     // Commit files as Initial Commit
-    await git().commit('updated').then(
+    await simpleGit().commit('updated').then(
       (successCommit) => {
         console.log("successCommit",successCommit);
     }, (failed) => {
@@ -103,7 +102,7 @@ async function getStoryblok(req, res, next){
     });
 
     // Finally push to online repository
-    await git().push('origin','main').then((success) => {
+    await simpleGit().push('origin','main').then((success) => {
         console.log(success,'successfully pushed');
     },(failed)=> {
         console.log(failed,'push failed');
@@ -134,7 +133,6 @@ app.get('/*', function(req, res, next) {
       version: 'draft'
     })
     
-    
     .then((response) => {
 
       // writeFileSync(__dirname + '/response/' + path + '.json', JSON.stringify(response))
@@ -142,6 +140,7 @@ app.get('/*', function(req, res, next) {
       res.render('index', {
         story: response.data.story
       });
+      ////////////render from storyblok/////////////////
       getStoryblok(req, res, next)
       
     })
